1270527354256493be473d48388789968ab9c404
Carlitos-madrigal83 <madrigalgallego.jc@gmail.com>
2021-06-22 13:56:21 +0200

    Creando funciones

--- Proyectos/0_2Contactos/script.js
+++ Proyectos/0_2Contactos/script.js
@@ -8,44 +8,79 @@ let dictionaryColors = {
     finishProgram: 'color:#ef476f'
 }
 
+function createContact(){
+
+}
+
+function modifyContact(){
+
+}
+
+function showDataContact(){
+
+}
+
+function showAllContacts(){
+
+}
+
+function removeContact(){
+
+}
+
+function bridgeExecuteInstruction(titleMenu, menu){ // Argumentos
+    
+    console.log('%c' + titleMenu, dictionaryColors.titleMenu);
+    console.log(menu);
+}
+
 function aplicationContactExecution() {
-
-
 
     // console.clear();
     let optionUser;
+    let showMessageReply = false;
 
     do {
-        console.log('%cMenu principal', dictionaryColors.titleMenu);
-        console.log('1 - Crear contacto \n2 - Modificar contacto \n3 - Mostrar contacto \n4 - Eliminar contacto \n5 - Salir de la aplicación (cancelar)');
+        if (!showMessageReply) {
+            console.log('%cMenu principal', dictionaryColors.titleMenu);
+        } else {
+            showMessageReply = !showMessageReply;
+        }
+
+        console.log('1 - Crear contacto\n2 - Modificar contacto\n3 - Mostrar contacto\n4 - Eliminar contacto\n5 - Salir de la aplicación (cancelar)');
         optionUser = prompt('Elija una de estas opciones');
+        console.clear();
 
         switch (optionUser) {
             case '1':
-                console.log('Cargando crear contacto');
+                bridgeExecuteInstruction('Ingresar contacto', '1 - Ingresar nuevo contacto\n2 - Volver menú principal (Cancelar)'); // Parametros
+                
 
                 break;
 
             case '2':
-                console.log('Cargando modificar contacto');
+                bridgeExecuteInstruction('Modificar contacto');
 
                 break;
 
             case '3':
-                console.log('Cargando mostrar contacto');
+                bridgeExecuteInstruction('Mostrar contacto');
 
                 break;
 
             case '4':
-                console.log('Cargando eliminar contacto');
+                bridgeExecuteInstruction('Eliminar contacto');
 
                 break;
 
             default:
+                showMessageReply = true;
 
                 if (optionUser === '') {
+                    console.log('%cMenu principal', dictionaryColors.titleMenu);
                     console.log('%cNo has añadido nada cacho cabrón', dictionaryColors.messageError);
                 } else if (optionUser !== '5' && optionUser !== null) {
+                    console.log('%cMenu principal', dictionaryColors.titleMenu);
                     console.log('%cEl valor introducido no se encuentra dentro de las opciones', dictionaryColors.messageError);
                 } else {
                     console.log('%cAplicación finalizada', dictionaryColors.messageError);
