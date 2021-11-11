a5839ea3f910e1bd35f48d34e3f1c654b5c2a1c2
Carlitos-madrigal83 <madrigalgallego.jc@gmail.com>
2021-11-11 14:21:21 +0100

    Generando devolverCombinacionChampionsLeague.js

---
+++ CodingChalleng/SyllabusSpecific/Functions/0_2DevolverCombinacionChampionsLeague.js
@@ -0,0 +1,23 @@
+// Crear un array con los equipos que compiten en la ChapionsLeague. Generar una función llamada
+// devolverCombinacionChampionsLeague() autoejecuta, encargada
+// de generar la tabla de competiciones entre equipos (uno contra uno). // 10 equipos -> 5 partidos.
+// El array debe por cada partido 
+
+// ["Real Madrid", "FC Barcelona", "Liverpool", "Manchester City"];
+// Números aleatorios: 0 y 3 -> "Real Madrid" contra "Manchester City"
+// ["FC Barcelona", "Liverpool"];
+// Números aleatorios: 0 y 1 -> "FC Barcelona" contra "Liverpool"
+
+// C O N S O L A --->
+// Equipos por jugar: Real Madrid - FC Barcelona - Liverpool - Manchester City
+// Partido 1º: Real Madrid VS Liverpool
+
+// Equipos por jugar: FC Barcelona - Manchester City
+// Partido 2º: FC Barcelona VS Manchester City
+
+// Excepciones:
+// Tener en cuenta que no puede un equipo competir contra si mismo. (verificarAleatorio())
+// Verificar que no se repiten equipos en el array. (verificarEquiposNoRepetidos())
+
+// Funciones extra
+// - mapearArray() -> encargada de eliminar los equipos que ya han competido
