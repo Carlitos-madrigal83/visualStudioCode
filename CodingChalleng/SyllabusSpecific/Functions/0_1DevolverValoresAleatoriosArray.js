dc6f56d43aee8416cb4084985bc27f8c40602ddf
Carlitos-madrigal83 <madrigalgallego.jc@gmail.com>
2021-11-11 13:36:02 +0100

    Generando 0_1DevolverValoresAleatoriosArray

---
+++ CodingChalleng/SyllabusSpecific/Functions/0_1DevolverValoresAleatoriosArray.js
@@ -0,0 +1,10 @@
+// Devolver de un array con nombres, dichos nombres de forma aleatoria por cada recarga del navegador.
+// Para hacer esto, hay que crear una función llamada "devolverValoresAleatoriosArray" la cual tiene que 
+// recibir solo un valor, el array con el que operar. La función deberá devolver el resultado.
+
+
+// Función definición
+
+// Función expresión
+
+// Función flecha (arrow function)
