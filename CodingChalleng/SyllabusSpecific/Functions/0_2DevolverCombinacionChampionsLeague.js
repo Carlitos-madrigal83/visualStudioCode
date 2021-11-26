7fa0e798abe1f40def7c5b076831b3108fa599b1
Carlitos-madrigal83 <madrigalgallego.jc@gmail.com>
2021-11-26 20:00:16 +0100

    continuando con la champions

--- CodingChalleng/SyllabusSpecific/Functions/0_2DevolverCombinacionChampionsLeague.js
+++ CodingChalleng/SyllabusSpecific/Functions/0_2DevolverCombinacionChampionsLeague.js
@@ -16,24 +16,130 @@
 // Partido 2º: FC Barcelona VS Manchester City
 
 // Excepciones:
-// Tener en cuenta que no puede un equipo competir contra si mismo. (verificarAleatorio())
+// Tener en cuenta que no puede un equipo competir contra si mismo y ninguno ya agregado
+// a la competición contra el local. (verificarAleatorio())
 // Verificar que no se repiten equipos en el array. (verificarEquiposNoRepetidos())
 
 // Funciones extra
 // - mapearArray() -> encargada de eliminar los equipos que ya han competido
 
-let teams = ["Real Madrid", "FC Barcelona", "Liverpool", "Manchester City", "Atlético de Madrid", "Sevilla CF", "Juventus", "Bayern de Munich"];
+let gruposChampionsLeague = {
+    "grupoA": ["Real Madrid", "FC Barcelona", "Liverpool", "Manchester City"],
+    "grupoB": ["2", "2", "2", "2"],
+    "grupoC": ["3", "3", "3", "3"],
+};
+//"Atlético de Madrid", "Sevilla CF", "Juventus", "Bayern de Munich"];
+
+
+/*
+
+    championsLeague = {
+        grupoA: {
+            "equipos": ["Real Madrid", "FC Barcelona", "Liverpool", "Manchester City"],
+            "partidos": {
+                "Real Madrid": {
+                    ida: {
+                        "FC Barcelona": 1-5,
+                        "Liverpool": 2-4,
+                        "Manchester City": 3-0
+                    },
+                    vuelta: {
+                        "FC Barcelona": 2-6,
+                        "Liverpool": 2-1,
+                        "Manchester City": 2-0
+                    }
+    
+                },
+                "FC Barcelona": {
+                    ida: {
+                        "Real Madrid": 2-6,
+                        "Liverpool": -,
+                        "Manchester City": -
+                    },
+                    vuelta: {
+                        "Real Madrid": 1-5,
+                        "Liverpool": -,
+                        "Manchester City": -
+                    }
+                },
+                "Liverpool": {
+                    ida: {
+                        "Real Madrid": 2-1,
+                        "FC Barcelona": -,
+                        "Manchester City": -
+                    },
+                    vuelta: {
+                        "Real Madrid": 2-4,
+                        "FC Barcelona": -,
+                        "Manchester City": -
+                    }
+                },
+                "Manchester City": {
+                    ida: {
+                        "Real Madrid": 2-0,
+                        "FC Barcelona": -,
+                        "Liverpool": -
+                    },
+                    vuelta: {
+                        "Real Madrid": 3-0,
+                        "FC Barcelona": -,
+                        "Liverpool": -
+                    }
+                }
+            }
+        },
+        grupoB: {...},
+        grupoC: {...},
+        ...
+        grupoH: {...}
+    }
+*/
+
 
 function devolverCombinacionChampionsLeague(arrTeams) {
-    let teamsRandom = Math.round(Math.random() * (arrTeams.length - 1));
-    for (let i = 0; i < teams.length; i++) {
-        let partidos =
-            console.log(teams[i] + " vs " + teams[i]);
 
+    const championsLeague = {};
+
+    let grupos = Object.keys(gruposChampionsLeague);
+    for (let i = 0; i < grupos.length; i++) {
+        championsLeague[grupos[i]] = {
+            "equipos": [],
+            "partidos": {}
+        };
     }
-    console.log(teamsRandom);
-    console.log(arrTeams[teamsRandom] + " VS " + arrTeams[teamsRandom]);
-}
-devolverCombinacionChampionsLeague(teams);
 
-    // Tienes que generar de forma aleatoria los números del array,( índices ) de 0 a 5
+
+
+
+
+
+
+
+
+
+
+    console.log({ championsLeague });
+    /*let partidos = 3;
+
+
+
+
+
+    do {
+        let teamsRandom = Math.trunc(Math.random() * arrTeams.length);
+        let
+
+        if (verificarAleatorio(arrTeams, teamsRandom, arrTeams[0])) {
+            console.log(arrTeams[0] + " vs " + arrTeams[teamsRandom]);
+            partidos--;
+        }
+
+    } while (partidos > 0);*/
+
+}
+devolverCombinacionChampionsLeague(gruposChampionsLeague);
+
+function verificarAleatorio(grupo, numRandom, equipoLocal) {
+    return (equipoLocal !== grupo[numRandom]) ? true : false
+};
+
