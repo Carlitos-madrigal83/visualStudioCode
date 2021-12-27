7568604c50c09899d164b3a8da9a5406ab6edbc2
carlitos_madrigal <madrigalgallego.jc@gmail.com>
2021-12-27 12:47:32 +0100

    Pendiente de finalizar function countLeaves

--- CodingChalleng/Challenges/1_2RemainingLeaves/script.js
+++ CodingChalleng/Challenges/1_2RemainingLeaves/script.js
@@ -1,6 +1,6 @@
 const width = 4;
 const height = 4;
-const winds = 'RRD';
+const winds = 'RRUU';
 const leaves = [
   [0, 0, 0, 0],
   [0, 0, 2, 0],
@@ -25,18 +25,63 @@ function capturingPositionsAndValues(leaves) {
 };
 
 function applingMovementsLeaves(posLeaves, winds) {
+  posLeaves = JSON.parse(JSON.stringify(posLeaves));
 
+    for (let i = 0; i < posLeaves.length; i++) {
+
+        for (let j = 0; j < winds.length; j++) {
+          if (winds[j] === 'R' || winds[j] === 'L') {// Columna
+            
+            if (winds[j] === 'R') {
+              posLeaves[i][1] = posLeaves[i][1] + 1;
+            }else if(winds[j] === 'L'){
+              posLeaves[i][1] = posLeaves[i][1] - 1; 
+            }
+
+          }else if(winds[j] === 'U' || winds[j] === 'D'){// Fila
+            
+            if (winds[j] === 'U') {
+              posLeaves[i][0] = posLeaves[i][0] - 1;
+            }else if(winds[j] === 'D'){
+              posLeaves[i][0] = posLeaves[i][0] + 1;
+            }
+          }
+        }
+      //posLeaves[i]
+      // [1, 2, 2]
+      // ( R ) -> [1, 3, 2] -> fila?columna, +1?-1
+      // ( R ) -> [1, 4, 2] -> fila?columna, +1?-1
+      // ( D ) -> [2, 4, 2] -> fila?columna, +1?-1
+    }
+    return posLeaves;
 };
 
-/*
-posLeaves = [
-  [leaves[i], leaves[i][j], leaves[i][j]],
-  [1, 2, 2],
-  [2, 1, 1],
-  [2, 2, 1],
-  ...,
-]
-*/
+function restoreLeaves(modifyPosLeaves, leaves){
+  leaves = JSON.parse(JSON.stringify(leaves));
+
+  for (let i = 0; i < leaves.length; i++) {
+    
+    for (let j = 0; j < leaves[i].length; j++) {
+      let valueFind = false;
+      console.log(leaves[i][j], "( " + i + ", " + j + " )");
+
+      for (let k = 0; k < modifyPosLeaves.length; k++) {
+        if (!valueFind && i === modifyPosLeaves[k][0] && j === modifyPosLeaves[k][1]) {
+          leaves[i][j] = modifyPosLeaves[k][2];
+          valueFind = true;
+        }else if(!valueFind){
+          leaves[i][j] = 0;
+        }
+      }
+    }
+  }
+
+  return leaves;
+}
+
+function countLeaves(leaves) {
+  
+}
 
 function main(width, height, leaves, winds) {
   console.log(width);   
@@ -46,8 +91,16 @@ function main(width, height, leaves, winds) {
 
   const posLeaves = capturingPositionsAndValues(leaves);
   console.log({posLeaves});
-  
+  console.log(typeof posLeaves);
+
   const modifyPosLeaves = applingMovementsLeaves(posLeaves, winds);
+  console.log({modifyPosLeaves})
+
+  const leavesRestore = restoreLeaves(modifyPosLeaves, leaves);
+  console.log({leavesRestore});
+
+  const numLeaves = countLeaves(leaves);
+
 }
 main(width, height, leaves, winds);
 
